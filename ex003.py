39818b83d7d5d96b2894b9d49a48d76068231e1d
Matheus <matheusprogramao2003@gmail.com>
2022-02-21 17:24:07 -0400

    adiciona o exercício 3

---
+++ ex003.py
@@ -0,0 +1,13 @@
+n1 = int(input('Valor Um: '))
+n2 = int(input('Valor Dois: '))
+soma = n1+n2
+sub = n1-n2
+mult = n1*n2
+e = n1**n2
+divi = n1 / n2
+print(f'A soma: {soma}' )
+print(f'subtração: {sub}')
+print(f'multiplicação: {mult}')
+print(f'e o exponencial é: {e} ')
+print('Divisão: {:.3f}'.format(divi))
+
