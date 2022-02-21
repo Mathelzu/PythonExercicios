57e6e45f930419f8a9720ad0faeab5057a7247b1
Matheus <matheusprogramao2003@gmail.com>
2022-02-21 11:16:52 -0400

    adiciona o exercício 1

---
+++ ex001.py
@@ -0,0 +1,6 @@
+algo = input('Digite Algo: ')
+print('você escreveu ',algo)
+print('Você escreveu o tipo: ', type(algo))
+print('Ele é do tipo alfanúmerico? ', algo.isalnum())
+print('Ele é do tipo númerico? ', algo.isnumeric())
+print('Ele é do tipo alfabético? ', algo.isalpha())
