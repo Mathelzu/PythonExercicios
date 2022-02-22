4147994653a8023ec0d37bde523f43ba8467d0fb
Matheus <matheusprogramao2003@gmail.com>
2022-02-22 18:03:40 -0400

    adiciona ao exercício 5 uma #

--- ex005.py
+++ ex005.py
@@ -3,4 +3,5 @@ n1 = float(input('Insira um número: '))
 dobro = n1 * 2
 triplo= n1 * 3
 raiz = math.sqrt(n1)
+#ou no lugar d import math, podeia usar a formila ex: raiz = n1 ** (1/2)
 print ( 'Dobro:{:.0f} \nTriplo: {:.0f} \nRaiz Quadrada: {:.2f} '.format(dobro , triplo , raiz))
