11af79a45b5901bf5452466fe3b220e51c0b909a
Matheus <matheusprogramao2003@gmail.com>
2022-02-22 14:17:14 -0400

    adiciona o exercício 7

---
+++ ex007.py
@@ -0,0 +1,4 @@
+metro = float(input('Insira o valor em Metros: '))
+cent = metro * 100
+mili = metro * 1000
+print('O valor em Centímetros é: {:.3f} \nO valor em Milímetros é: {:.3f}'.format(cent , mili) )
