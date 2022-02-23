fa622a5ec6ff9a445202529d052d97c4634d870d
Matheus <matheusprogramao2003@gmail.com>
2022-02-23 17:26:44 -0400

    adiciona o exercício 15

---
+++ ex015.py
@@ -0,0 +1,6 @@
+import math
+num = float(input('Digite um número: '))
+inteiro = math.trunc(num)
+print(f'O número {num} tem o valor inteiro de {inteiro}') 
+#num = float (input('Digite um numero: '))
+#print(f'O número {num} tem o valor inteiro de {int(num)}')
