7d6b73eed9a4ea9df02c83bcfbd9602c5b7c966f
Matheus <matheusprogramao2003@gmail.com>
2022-02-23 11:38:23 -0400

    adiciona o exercício 12

---
+++ ex012.py
@@ -0,0 +1,4 @@
+salario = float(input('Seu salário atual é de: '))
+novoSalario = (salario*15)/100
+valorFinal = salario + novoSalario
+print(f'Com o aumento de 15% seu salario vai para {valorFinal:.2f}')
